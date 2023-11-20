29be3167fd1defa9dabac0baf443eabed264c992
VannWasHere <ateen1203@gmail.com>
2023-11-20 13:23:39 +0700

    Add use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './css/index.css'
-import NavigationBar from './components/NavigationBar';
+import NavigationBar from './components/NavigationBar'
+import {useState} from 'react'
 
 const App = () => {
-  return (
-    <>
-      <NavigationBar />
-      <main>
-        
-      </main>
-    </>
-  );
+    const [getButtonClickValue, setButtonClickValue] = useState("");
+
+    const changeNavTitle = () => {
+        setButtonClickValue("My React Journey")
+    }
+
+
+    return(
+        <>
+            <NavigationBar name={"Justine Donovan"} navTitle={getButtonClickValue}/>
+            <main className='mt-8 p-5'>
+                <button className='bg-slate-800 text-white p-2 w-40 rounded-lg transition-all hover:bg-slate-600'
+                onClick={() => changeNavTitle()}>Click Me</button>
+                <hr />
+
+            </main>
+        </>
+    );
 }
 
 export default App;
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,10 +1,13 @@
+import PropTypes from 'prop-types';
+
 const menuItems = ['Home', 'Pages', 'About'];
 
-const NavigationBar = () => {
+const NavigationBar = (props) => {
+    console.log(props);
     return (
         <header>
             <nav className="flex justify-between bg-slate-600 w-full p-12 text-white">
-                <h1 className="text-3xl font-bold tracking-wider uppercase">My react journey</h1>
+                <h1 className="text-3xl font-bold tracking-wider uppercase">{!props.navTitle ? "My Javascript Journey" : props.navTitle}</h1>
                 <div>
                     <ul className="flex space-x-4 w-full text-lg font-bold">
                     {/* Making list with array map function */}
@@ -15,10 +18,24 @@ const NavigationBar = () => {
                     ))}
                     </ul>
                 </div>
-                <h2 className="text-2xl">Justine Donovan</h2>
+                { props.name == "Anonymous" ? (
+                    <h2 className='text-2xl text-black'>{props.name}</h2>
+                ) : (
+                    <h2 className='text-2xl'>Hei, {props.name} !</h2>
+                )}
             </nav>
         </header>
     )
+}
+
+NavigationBar.propTypes = {
+    name: PropTypes.string,
+    navTitle: PropTypes.string,
+}
+
+NavigationBar.defaultProps = {
+    name: "Anonymous",
+    navTitle: "My Javascript Journey"
 }
 
 export default NavigationBar;
