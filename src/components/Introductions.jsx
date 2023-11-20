3b549a38eeb1bbbfbf525feb828bd20144ca745c
VannWasHere <ateen1203@gmail.com>
2023-11-20 11:43:26 +0700

    Disabling ESLINT - Prop-types

--- src/components/Introductions.jsx
+++ src/components/Introductions.jsx
@@ -1,22 +1,10 @@
-import PropTypes from 'prop-types';
-
 const Introduction = (props) => {
     return (
-        <div className="mt-12">
+        <div className="mt-5">
             <h1 className='text-2xl'>Halo nama saya, <b>{props.name}</b>. Age: {props.age}</h1>
         </div>
     );
 }
-
-Introduction.propTypes = {
-    name: PropTypes.string,
-    age: PropTypes.string,
-};
-
-Introduction.defaultProps = {
-    name: "Kurniawan",
-    age: "20",
-};
 
 
 export default Introduction;
