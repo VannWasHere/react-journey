9320c19de57bca3d1d67892a5059efb4e5196aeb
VannWasHere <ateen1203@gmail.com>
2023-11-21 21:19:34 +0700

    Test Git

--- src/ComponentDicoding/AddContact.jsx
+++ src/ComponentDicoding/AddContact.jsx
@@ -4,6 +4,7 @@ const AddContact = () => {
     return (
         <div className="mt-7 p-2">
             <h1 className="text-3xl font-bold tracking-wider">Contact Appliaction</h1>
+            <h1>Test</h1>
             <Button buttonText={"Add Person"}/>
         </div>
     );
