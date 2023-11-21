c81b82fb47dc3dd71c5ea5048adbaf7f5c1af80c
VannWasHere <ateen1203@gmail.com>
2023-11-21 20:46:02 +0700

    Replacing AddContact Components

---
+++ src/ComponentDicoding/AddContact.jsx
@@ -0,0 +1,12 @@
+import Button from '../components/Button'
+
+const AddContact = () => {
+    return (
+        <div className="mt-7 p-2">
+            <h1 className="text-3xl font-bold tracking-wider">Contact Appliaction</h1>
+            <Button buttonText={"Add Person"}/>
+        </div>
+    );
+}
+
+export default AddContact;
