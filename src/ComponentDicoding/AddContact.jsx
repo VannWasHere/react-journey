b1939230bacb0952b07c4650aea06ecd76b9ca70
VannWasHere <ateen1203@gmail.com>
2023-11-23 08:49:04 +0700

    Small Delete

--- src/ComponentDicoding/AddContact.jsx
+++ src/ComponentDicoding/AddContact.jsx
@@ -5,7 +5,6 @@ import {getData} from '../Data/data'
 
 const AddContact = () => {
     const containData = getData();
-    console.log(containData);
     return (
         <div className="mt-7 border p-4">
             <h1 className="text-3xl font-bold tracking-wider">Contact Appliaction</h1>
